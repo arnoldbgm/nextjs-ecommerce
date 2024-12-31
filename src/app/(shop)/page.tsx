adf52c3253e719f5b72527902fd2937605a986b0
arnoldbgm <arnold.gallegosm@gmail.com>
2024-12-31 16:52:27 -0500

    ✨Frontend: Implementacion de Grid

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,16 @@
 import { Title } from "@/components";
+import { ProductGrid } from "@/components/products/product-grid/ProductGrid";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products;
+
 
 export default function ShopPage() {
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
+
+      <ProductGrid products={products} />
     </>
   )
 }
